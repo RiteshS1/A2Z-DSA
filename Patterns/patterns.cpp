900251b21189545b82534a5bfb87b91961ac7d78
RS <rsphenomenal@gmail.com>
2025-01-05 18:34:10 +0530

    pattern2

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -15,12 +15,33 @@ void pattern1( int n ){
         cout<<endl;
     }
 }
+void pattern2( int n ){
+    /*
+    
+    *
+    **
+    ***
+    ****
+    
+    
+    */
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+}
+
 
 int main()
 {
     int n;
     cin>>n;
     pattern1(n);
+    pattern2(n);
 
     return 0;
 }
