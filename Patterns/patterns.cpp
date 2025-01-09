56d751c148e9435c48b522a53657a91a2b838d5e
RS <rsphenomenal@gmail.com>
2025-01-09 23:41:30 +0530

    new patterns

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -93,9 +93,46 @@ void pattern5( int n ){
     }
   
 }
+void pattern6(int n){
+    /*
+       *
+      * *
+     * * *
+    * * * *
+   * * * * *
+    
+    */
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<i+1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
 
+}
 
-
+void pattern7(int n){
+    /*
+    *
+   ***
+  *****
+ ******* 
+ 
+    */
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+   
+   }
 int main()
 {
     int n;
@@ -105,7 +142,8 @@ int main()
     pattern3(n);
     pattern4(n);
     pattern5(n);
-    
+    pattern6(n);
+    pattern7(n);
 
     return 0;
 }
